aa88e1a6f9fe3adcb7b32b7aa2d81d68a019c1b7
Elena Denisova <lenaden@gmail.com>
2022-03-11 15:38:57 -0500

    Initial commit

---
+++ src/main/java/ru/netology/AfishaManager.java
@@ -0,0 +1,34 @@
+package ru.netology;
+
+public class AfishaManager {
+    private int lentaSize = 10;
+    private MovieEntry[] movies = new MovieEntry[0];
+
+    public AfishaManager() {
+    }
+
+    public AfishaManager(int lentaSize) {
+        this.lentaSize = lentaSize;
+    }
+
+    public void add(MovieEntry newMovie) {
+        //отчего бы не хранить фильмы в массиве movies в уже инвертированном порядке?
+        //т.е. ранее добавленные - сдвинем на 1 позицию вперед, а новый фильм запишем по индексу 0.
+        // Это заметно упростит код getLenta()
+        int newLength = movies.length + 1;
+        MovieEntry[] modified = new MovieEntry[newLength];
+        //сдвигаем уже имеющиеся
+        System.arraycopy(movies, 0, modified, 1, movies.length);
+        //новейший довабляем в нулевой индекс
+        modified[0] = newMovie;
+        movies = modified;
+    }
+
+    public MovieEntry[] getLenta() {
+        int lentaLength = (movies.length > lentaSize) ? lentaSize : movies.length;
+        MovieEntry[] lenta = new MovieEntry[lentaLength];
+        System.arraycopy(movies, 0, lenta, 0, lentaLength);//фильмы в массиве movies уже инвертированы!
+        return lenta;
+    }
+
+}
---
+++ src/test/java/ru/netology/AfishaManagerTest.java
@@ -0,0 +1,117 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AfishaManagerTest {
+    private AfishaManager am;
+    private MovieEntry movie001;
+    private MovieEntry movie002;
+    private MovieEntry movie003;
+    private MovieEntry movie004;
+    private MovieEntry movie005;
+    private MovieEntry movie006;
+    private MovieEntry movie007;
+    private MovieEntry movie008;
+    private MovieEntry movie009;
+    private MovieEntry movie010;
+    private MovieEntry movie011;
+    private MovieEntry movie012;
+    private MovieEntry movie013;
+    private MovieEntry movie014;
+    private MovieEntry movie015;
+
+    @BeforeEach
+    public void setUp() {
+        movie001 = new MovieEntry(1, "Terminator", "Action", "term.png", false);
+        movie002 = new MovieEntry(2, "Rocky", "Action", "rocky.png", false);
+        movie003 = new MovieEntry(3, "First Blood", "Action", "fb.png", false);
+        movie004 = new MovieEntry(4, "Cobra", "Action", "cbr.png", false);
+        movie005 = new MovieEntry(5, "Bananas", "Action", "bananas.png", false);
+        movie006 = new MovieEntry(6, "Assassins", "Action", "ass.png", false);
+        movie007 = new MovieEntry(7, "Demolition Man", "Action", "dm.png", false);
+        movie008 = new MovieEntry(8, "Rambo", "Action", "rambo.png", false);
+        movie009 = new MovieEntry(9, "Creed", "Action", "creed.png", false);
+        movie010 = new MovieEntry(10, "Homefront", "Action", "hf.png", false);
+        movie011 = new MovieEntry(11, "Alien", "Sci-Fi", "aln.png", false);
+        movie012 = new MovieEntry(12, "Gravity", "Sci-Fi", "gvt.png", false);
+        movie013 = new MovieEntry(13, "Event Horizon", "Goofy", "aln.png", false);
+        movie014 = new MovieEntry(14, "Shrek", "Cartoons", "shr.png", false);
+        movie015 = new MovieEntry(15, "Scooby-Doo", "Cartoons", "scbd.png", false);
+    }
+
+    @Test
+    public void shouldAddManyUsingDefaultLentaSize() {
+        am = new AfishaManager();
+        am.add(movie001);
+        am.add(movie002);
+        am.add(movie003);
+        am.add(movie004);
+        am.add(movie005);
+        am.add(movie006);
+        am.add(movie007);
+        am.add(movie008);
+        am.add(movie009);
+        am.add(movie010);
+        am.add(movie011);
+        am.add(movie012);
+        am.add(movie013);
+        am.add(movie014);
+        am.add(movie015);
+
+        MovieEntry[] expected = new MovieEntry[]{movie015, movie014, movie013, movie012, movie011, movie010, movie009, movie008, movie007, movie006};
+        assertArrayEquals(expected, am.getLenta());
+    }
+
+    @Test
+    public void shouldAddManyUsingCustomLentaSize() {
+        am = new AfishaManager(4);
+        am.add(movie001);
+        am.add(movie002);
+        am.add(movie003);
+        am.add(movie004);
+        am.add(movie005);
+        am.add(movie006);
+        am.add(movie007);
+        am.add(movie008);
+        am.add(movie009);
+        am.add(movie010);
+
+        MovieEntry[] expected = new MovieEntry[]{movie010, movie009, movie008, movie007};
+        assertArrayEquals(expected, am.getLenta());
+    }
+
+    @Test
+    public void shouldAddFewUsingDefaultLentaSize() {
+        am = new AfishaManager();
+        am.add(movie001);
+        am.add(movie002);
+        am.add(movie003);
+        am.add(movie004);
+        am.add(movie005);
+
+        MovieEntry[] expected = new MovieEntry[]{movie005, movie004, movie003, movie002, movie001};
+        assertArrayEquals(expected, am.getLenta());
+    }
+
+    @Test
+    public void shouldAddFewUsingCustomLentaSize() {
+        am = new AfishaManager(6);
+        am.add(movie001);
+        am.add(movie002);
+        am.add(movie003);
+
+        MovieEntry[] expected = new MovieEntry[]{movie003, movie002, movie001};
+        assertArrayEquals(expected, am.getLenta());
+    }
+
+    @Test
+    public void shouldAddNothing() {
+        am = new AfishaManager();
+        MovieEntry[] expected = new MovieEntry[0];
+        assertArrayEquals(expected, am.getLenta());
+    }
+}
